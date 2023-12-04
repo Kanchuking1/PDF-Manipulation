c882372d1003ab006609b9ef0dbe10533ab0ffd3
Wahib Kapdi <kanchuking1@gmail.com>
2023-12-04 11:32:25 +0530

    Python PDF Merger Created

---
+++ pdf_append.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+import sys
+try:
+    from PyPDF2 import PdfReader, PdfWriter
+except ImportError:
+    from pyPdf import PdfFileReader, PdfFileWriter
+
+def pdf_cat(input_files, output_stream):
+    input_streams = []
+    try:
+        # First open all the files, then produce the output file, and
+        # finally close the input files. This is necessary because
+        # the data isn't read from the input files until the write
+        # operation. Thanks to
+        # https://stackoverflow.com/questions/6773631/problem-with-closing-python-pypdf-writing-getting-a-valueerror-i-o-operation/6773733#6773733
+        for input_file in input_files:
+            input_streams.append(open(input_file, 'rb'))
+        writer = PdfWriter()
+        for reader in map(PdfReader, input_streams):
+            for n in range(len(reader.pages)):
+                writer.add_page(reader.pages[n])
+        writer.write(output_stream)
+    finally:
+        for f in input_streams:
+            f.close()
+        output_stream.close()
+
+if __name__ == '__main__':
+    if sys.platform == "win32":
+        import os, msvcrt
+        msvcrt.setmode(sys.stdout.fileno(), os.O_BINARY)
+    output_stream = open('result.pdf', 'w+b')
+    pdf_cat(sys.argv[1:], output_stream)
